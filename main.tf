f6aba076f9b5fc4a82f31bc1bdef2de2afae5de3
qingmin.chen@accenture.com <chenqingmin@Chens-MacBook-Pro.local>
2021-06-25 18:59:21 +0800

    add wrong code

--- main.tf
+++ main.tf
@@ -5,9 +5,6 @@ provider "google" {
   credentials = file("test-scheduler-309908-9a714b3df80c.json")
 }
 
-data "google_composer_image_versions" "all" {
-}
-
 resource "google_pubsub_topic" "create_destroy_topic" {
   name = "notify_destory_topic"
 }
